91b4dbbd0f115704a70f388de22859d07b3950dc
nicholasfeigelson <nicholasfeigelson@localhost>
2022-09-22 21:50:41 -0500

    Finished Version of Prime Numbers

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -3,7 +3,6 @@ package studio3;
 import java.util.Scanner;
 
 public class Sieve {
-
 	public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 System.out.println("Enter value for n");
@@ -11,20 +10,23 @@ int n = in.nextInt();
 boolean[] prime;
 int i=0;
 prime =  new boolean [n];
-
 for(i=0;i<n;i++)
-{
-	if (i%2==0 || i%3==0 || i%4==0 || i%5==0 ||i%7==0)
+	if (i==2|| i==3 || i==5 || i==7)
+	{
+		System.out.println(i);
+	}
+	else
+	{
+	if (i%2==0 || i%3==0 || i%4==0 || i%5==0 ||i%7==0 || i==1)
 	{
 		prime[i]=false;
 	}
 	else
 	{
-		prime[i] = true;
 		System.out.println(i);
 	}
 }
 	}
-	
-}
+	}	
+
 
